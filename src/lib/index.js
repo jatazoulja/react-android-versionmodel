2f8d60c258008f69ddfd14319ebd3a14b0055eb7
petereman.abastillas <jatazoulja@gmail.com>
2018-06-13 17:11:37 +0800

    removing consoles.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,9 @@
 import AndroidDeviceList from "./utils/device-list";
 import _ from "lodash";
 function getByModel(modelName) {
-  console.log(AndroidDeviceList.lenght);
   return _.filter(AndroidDeviceList, ["model", modelName]);
 }
 function getByDevice(deviceName) {
-  console.log(AndroidDeviceList.lenght);
   return _.filter(AndroidDeviceList, ["device", deviceName]);
 }
 function getAll() {
